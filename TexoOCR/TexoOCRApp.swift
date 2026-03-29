8314bfb6909ce796f86a87ebd79a457badca2802
charlie <@MBP>
2026-03-29 14:21:52 +0800

    Initial Commit

---
+++ TexoOCR/TexoOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TexoOCRApp.swift
+//  TexoOCR
+//
+//  Created by charlie on 2026/3/29.
+//
+
+import SwiftUI
+
+@main
+struct TexoOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TexoOCRTests/TexoOCRTests.swift
@@ -0,0 +1,17 @@
+//
+//  TexoOCRTests.swift
+//  TexoOCRTests
+//
+//  Created by charlie on 2026/3/29.
+//
+
+import Testing
+@testable import TexoOCR
+
+struct TexoOCRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
